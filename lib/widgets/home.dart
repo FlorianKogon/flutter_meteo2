ad30ffb0a7a1ef6d3c63d262d233b5af47325b1d
Florian Kogon <F.Kogon@gmail.com>
2021-01-20 12:14:20 +0100

    drawer-added

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -12,14 +12,48 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  List<String> cities = ['Paris', 'Bordeaux', 'Marseille'];
+
+  String chosenCity = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Container(
+          color: Colors.blue,
+          child: ListView.builder(
+              itemCount: cities.length,
+              itemBuilder: (context, i) {
+                return ListTile(
+                  title: Text(
+                      cities[i],
+                      style: TextStyle(color: Colors.white)
+                  ),
+                  onTap: () {
+                    setState(() {
+                      chosenCity = cities[i];
+                      Navigator.pop(context);
+                    });
+                  },
+                  trailing: FlatButton(
+                    onPressed: () => {
+                      cities.removeAt(i)
+                    },
+                    child: Icon(
+                      Icons.delete,
+                      color: Colors.white,
+                    ),
+                  ),
+                );
+              }),
+        ),
+      ),
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
+        child: Text(chosenCity == null ? "Ville actuelle" : chosenCity
         ),
       ),
     );
