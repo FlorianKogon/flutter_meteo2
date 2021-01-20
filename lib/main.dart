cbccb00c15f91b8175fc6123442f613e3a026218
Florian Kogon <F.Kogon@gmail.com>
2021-01-20 18:08:39 +0100

    add-location

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,206 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_meteo/widgets/my_app.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:location/location.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Meteo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  String key = 'cities';
+
+  List<String> cities = [];
+
+  String chosenCity = '';
+
+  Location location;
+  LocationData locationData;
+  Stream<LocationData> streamData;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getSharedPreferences();
+    location = Location();
+    //getFirstLocation();
+    getCurrentLocation();
+  }
+
+
+  getFirstLocation() async {
+    try {
+      locationData = await location.getLocation();
+    } catch (e) {
+      print("Error $e");
+    }
+  }
+
+  getCurrentLocation() {
+    streamData = location.onLocationChanged;
+    streamData.listen((event) {
+      print("Nouvelle position : ${event.longitude} / ${event.latitude}");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: Container(
+          color: Colors.blue,
+          child: ListView.builder(
+              itemCount: cities.length + 2,
+              itemBuilder: (context, i) {
+                if (i == 0) {
+                  return DrawerHeader(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        customText("Mes villes", fontSize: 22.0),
+                        RaisedButton(
+                          elevation: 5.0,
+                          onPressed: addCity,
+                          color: Colors.teal,
+                          child: customText('Ajouter une ville'),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+                else if (i == 1) {
+                  return ListTile(
+                    title: customText("Ma ville actuelle"),
+                    onTap: () {
+                      setState(() {
+                        chosenCity = null;
+                        Navigator.pop(context);
+                      });
+                    },
+                  );
+                }
+                else {
+                  String city = cities[i - 2];
+                  return ListTile(
+                    title: customText(city),
+                    onTap: () {
+                      setState(() {
+                        chosenCity = city;
+                        Navigator.pop(context);
+                      });
+                    },
+                    trailing: FlatButton(
+                      onPressed: () => {
+                        deleteSharedPreferences(city)
+                      },
+                      child: Icon(
+                        Icons.delete,
+                        color: Colors.white,
+                      ),
+                    ),
+                  );
+                }
+              }),
+        ),
+      ),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: customText(
+            chosenCity == null ? "Ville actuelle" : chosenCity,
+            color: Colors.black87
+        ),
+      ),
+    );
+  }
+
+  Text customText(String data, {color: Colors.white, fontSize: 17.0, fontStyle: FontStyle.italic, textAlign: TextAlign.center} ) {
+    return Text(
+      data,
+      textAlign: textAlign,
+      style: TextStyle(
+          color: color,
+          fontSize: fontSize,
+          fontStyle: fontStyle
+      ),
+    );
+  }
+
+  Future<Null> addCity() async {
+    return showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            contentPadding: EdgeInsets.all(20.0),
+            title: customText("Ajouter une ville", fontSize: 22.0, color: Colors.teal),
+            children: [
+              TextField(
+                autofocus: true,
+                decoration: InputDecoration(
+                    labelText: "Nouvelle ville"
+                ),
+                onSubmitted: (String str) {
+                  saveSharedPreferences(str);
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
+        }
+    );
+  }
+
+  void getSharedPreferences() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    List<String> list = await sharedPreferences.getStringList(key);
+    if (list != null) {
+      setState(() {
+        cities = list;
+      });
+    }
+  }
+
+  void saveSharedPreferences(String str) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    cities.add(str);
+    await sharedPreferences.setStringList(key, cities);
+    getSharedPreferences();
+  }
+
+  void deleteSharedPreferences(String str) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    cities.remove(str);
+    await sharedPreferences.setStringList(key, cities);
+    getSharedPreferences();
+  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_meteo/widgets/my_app.dart';
+import 'package:flutter_meteo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
